9c6a234dbe5626b33a3178c1cdf6d52e515b0544
notsamicc <69369641+notsamicc@users.noreply.github.com>
2021-09-21 19:15:22 +0100

    Update

--- Gum.py
+++ Gum.py
@@ -1,12 +1,17 @@
 import time
+import winsound
 
 print("What do you call a bear with no teeth?")
 #waits X seconds before printing punchline
+winsound.Beep(1500, 2000)
 time.sleep(2)
+winsound.Beep(1500, 2000)
 print("A gummy bear!")
 
+winsound.Beep(1500, 2000)
 #waits Y seconds before "laughing" every Z seconds
 time.sleep(3)
 while True:
     time.sleep(.1)
+    winsound.Beep(1500, 100)
     print("haha")
